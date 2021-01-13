c53bf8af604c12352ff19da1c93789a22eded4e4
olatunjikazim <reachkazeem200@gmail.com>
2021-01-13 11:49:56 +0100

    feat

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log
+console.log("codevillage OOP class")
+
+
+let baseSalary = 30 _000;
+let incentive = 10
+let points = 30
+
+function getMonthlyPay(baseSalary, incentive, points) {
+    return baseSalary + incentive + points;
+}
+
+
+let employee = {
+    baseSalary: 40 _000,
+    incentive = 1000,
+    points = 290,
+    getMonthlyPay: function() {
+        return this.baseSalary + this.incentive + this.points
+    }
+
+
+
+        const rectangle = {
+
+        width: 10,
+        height: 10,
+        location: { x: 0, y: 0 },
+        draw() {
+            console.log(`drawing...`
+
+                spec)
+
+        }
+    }
+
+
+}
+
+const createName = (firstName, lastName) => ({
+
+})
+
+function rectangle(width, height) {
+    this.width: width;
+    this.height:
+}
